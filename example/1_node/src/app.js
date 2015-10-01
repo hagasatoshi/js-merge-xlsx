62cedd803025a16f08bc49d8a8f9492f6af07c23
shaga <shaga@pasonatquila.com>
2015-10-02 05:27:46 +0900

    update example1

--- example/1_node/src/app.js
+++ example/1_node/src/app.js
@@ -5,12 +5,12 @@
  * * @date 2015/09/30
  **/
 
-import ExcelMerge from '../../build/ExcelMerge'
+import ExcelMerge from 'js-merge-xlsx'
 import fs from 'fs'
 import JSZip from 'jszip'
 
 //Init template engine instance
-var excel_data = fs.readFileSync('./build_example/1_node/template/Template.xlsx');
+var excel_data = fs.readFileSync('./template/Template.xlsx');
 var merge = new ExcelMerge(new JSZip(excel_data));
 
 //Prepare binding-data
