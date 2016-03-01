1f17239e8d7b58cfd4de6aa048998879b7d60015
shaga <shaga@pasonatquila.com>
2016-03-02 00:59:43 +0900

    one-gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,11 +1,46 @@
 /**
- * * Gulp task definition
- * * @author Satoshi Haga
- * * @date 2015/09/30
- **/
-var gulp = require('gulp');
-var requireDir = require('require-dir');
+ * Gulp task definition
+ * @author Satoshi Haga
+ * @date 2015/09/30
+ */
+const gulp = require('gulp');
+const runSequence = require('run-sequence');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const mocha = require('gulp-mocha');
 
-//define gulp tasks from each gulpfile in './gulp'
-requireDir('./gulp');
+gulp.task('compile', ()=>{
+    return gulp.src('src/**/*.js')
+        .pipe(babel())
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('compress', ['compress-excelmerge', 'compress-lib']);
+gulp.task('compress-excelmerge', ()=>{
+    return gulp.src('./excelmerge.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./'));
+});
+gulp.task('compress-lib', ()=>{
+    return gulp.src('lib/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('lib'));
+});
+
+gulp.task('test-setup', ()=>{
+    return gulp.src('src_test/mocha/**/*.js')
+        .pipe(babel())
+        .pipe(gulp.dest('test/mocha'));
+});
+
+gulp.task('mocha', ()=>{
+    return gulp.src('test/mocha/test.js')
+        .pipe(mocha());
+});
+
+gulp.task('default',(cb)=>{
+    runSequence(
+        ['compile','test-setup'], 'compress', 'mocha', cb
+    )
+});
 
