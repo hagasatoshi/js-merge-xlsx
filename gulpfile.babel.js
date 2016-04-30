644d6a10c6932427dddac425ba40547244dedd10
shaga <shaga@pasonatquila.com>
2016-04-30 10:02:59 +0900

    set lint as first-task in default task sequence

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,60 +9,60 @@ const runSequence = require('run-sequence');
 
 const config = {
     js: {
-        src: 'src/**/*.js',
+        src:  'src/**/*.js',
         dest: './'
     },
     uglify: {
-        src: './excelmerge.js',
-        src_lib: 'lib/*.js',
-        dest: './',
+        src:      './excelmerge.js',
+        src_lib:  'lib/*.js',
+        dest:     './',
         dest_lib: 'lib'
     },
     test: {
-        src: 'src_test/mocha/**/*.js',
+        src:  'src_test/mocha/**/*.js',
         dest: 'test/mocha',
         main: 'test/mocha/test.js'
     }
 };
 
-gulp.task('compile', ()=>{
+gulp.task('compile', () => {
     return gulp.src(config.js.src)
         .pipe($.babel())
         .pipe(gulp.dest(config.js.dest));
 });
 
 gulp.task('compress', ['compress-excelmerge', 'compress-lib']);
-gulp.task('compress-excelmerge', ()=>{
+gulp.task('compress-excelmerge', () => {
     return gulp.src(config.uglify.src)
         .pipe($.uglify())
         .pipe(gulp.dest(config.uglify.dest));
 });
-gulp.task('compress-lib', ()=>{
+gulp.task('compress-lib', () => {
     return gulp.src(config.uglify.src_lib)
         .pipe($.uglify())
         .pipe(gulp.dest(config.uglify.dest_lib));
 });
 
-gulp.task('test-setup', ()=>{
+gulp.task('test-setup', () => {
     return gulp.src(config.test.src)
         .pipe($.babel())
         .pipe(gulp.dest(config.test.dest));
 });
 
-gulp.task('mocha', ()=>{
+gulp.task('mocha', () => {
     return gulp.src(config.test.main)
         .pipe($.mocha());
 });
 
-gulp.task('lint', ()=>{
+gulp.task('lint', () => {
     return gulp.src(config.js.src)
         .pipe($.eslint({useEslintrc: true}))
         .pipe($.eslint.format())
         .pipe($.eslint.failAfterError());
 });
-gulp.task('default',(cb)=>{
+gulp.task('default', (cb) => {
     runSequence(
-        ['compile','test-setup'], 'compress', 'mocha', cb
+        'lint', ['compile','test-setup'], 'compress', 'mocha', cb
     )
 });
 
