76791cf8e3851dbc5f363eeada3207fa12e5ebee
shaga <shaga@pasonatquila.com>
2016-05-23 01:57:51 +0900

    update cient JavaScript files of example of browser

--- example/2_express/src/js/client/controllers/index.js
+++ example/2_express/src/js/client/controllers/index.js
@@ -1,8 +1,5 @@
-/**
- * * index.js
- * * @author Satoshi Haga
- * * @date 2015/10/06
- **/
-
 module.exports = angular.module('app.controllers', [])
-.controller('indexController', require('./index_controller'));
+.controller(
+    'indexController',
+    require('./index_controller')
+);
